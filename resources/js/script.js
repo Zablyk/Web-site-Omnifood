7ec3dd8b042fa0af21ddb036173b43c29ca955cf
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-07-21 19:51:40 -0400

    added a animation which is scroll to section plans by clicking I'm hungry button

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
-
-    $('.js--section-features').waypoint(function(direction) {
-      if (direction == "down") {
-        $('nav').addClass('sticky');  
-      }  
-    else {
-        $('nav').removeClass('sticky');;
-        
-    }
-        
-    }, {
-  offset: '60px'
-});
     
+    
+    /* For the sticky navigation */
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '60px;'
+    });
+    
+    $('.js--scroll-to-plans').click(function() {
+       $('html, body').animate({scrollTop: $('.js--sections-plans').offset().top}, 1000); 
+    });
 
 });
